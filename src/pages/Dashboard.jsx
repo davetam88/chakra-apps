e74d71165afccafc681c69be9fd28a41c77c9d8e
David Tam <davetam88@gmail.com>
2024-02-12 17:20:15 -0500

    ch-02-e code

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,61 +1,39 @@
-import { EditIcon, ViewIcon } from "@chakra-ui/icons"
-import { 
-  Box, 
-  SimpleGrid,
-  Text,
-  Flex,
-  Heading,
-  Card, 
-  CardHeader,
-  CardBody,
-  CardFooter,
-  HStack,
-  Divider,
-  Button
-} from "@chakra-ui/react"
-import { useLoaderData } from "react-router-dom"
+import { Box, Container, Heading, Text } from "@chakra-ui/react";
 
 export default function Dashboard() {
-  const tasks = useLoaderData()
+  const boxStyles = {
+    p: "10px",
+    bg: "purple.400",
+    color: "white",
+    m: "10px",
+    textAlign: "center",
+    ":hover": {
+      color: "black",
+    },
+    filter: "blur(2px)",
+  };
 
   return (
-    <SimpleGrid spacing={10} minChildWidth={300}>
-      {tasks && tasks.map(task => (
-        <Card key={task.id} borderTop="8px" borderColor="purple.400" bg="white">
+    <Container as="section" maxWidth="4xl" py="20px">
+      <Heading my="30px" p="10px">
+        Chakra UI Components
+      </Heading>
 
-          <CardHeader color="gray.700">
-            <Flex gap={5}>
-              <Box w="50px" h="50px">
-                <Text>AV</Text>
-              </Box>
-              <Box>
-                <Heading as="h3" size="sm">{task.title}</Heading>
-                <Text>by {task.author}</Text>
-              </Box>
-            </Flex>
-          </CardHeader>
+      <Text marginLeft="30px">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur
+        minima impedit inventore facilis amet, doloremque repellat dicta officia
+      </Text>
 
-          <CardBody color="gray.500">
-            <Text>{task.description}</Text>
-          </CardBody>
+      <Text ml="30px" color="blue.300" fontWeight="bold">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur
+        minima impedit inventore facilis amet, doloremque repellat dicta officia
+      </Text>
 
-          <Divider borderColor="gray.200" />
+      <Box my="30px" p="20px" bg="orange">
+        <Text color="white">This is a Box</Text>
+      </Box>
 
-          <CardFooter>
-            <HStack>
-              <Button variant="ghost" leftIcon={<ViewIcon />}>Watch</Button>
-              <Button variant="ghost" leftIcon={<EditIcon />}>Comment</Button>
-            </HStack>
-          </CardFooter>
-
-        </Card>
-      ))}
-    </SimpleGrid>
-  )
-}
-
-export const tasksLoader = async () => {
-  const res = await fetch('http://localhost:3000/tasks')
-
-  return res.json()
+      <Box sx={boxStyles}>Hello, Ninjas!</Box>
+    </Container>
+  );
 }
